06340f74007c40e619853046f5ae7582a55d8659
Roman <student.rv86@gmail.com>
2019-05-06 21:44:06 +0300

    add CRUD methods

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,9 +1,8 @@
 package com.company;
 
 import com.company.api.ApiManager;
-import com.akadatsky.model.Post;
-import com.company.model.SendPostResult;
 import com.company.model.User;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -14,16 +13,20 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        requestPosts();
-        sendPost();
+        //getAllUsers();
+        //getUser(85);
+        //getUserByName("Victor");
+        //getUsersByAge(18, 30);
+        //addUser(new User("Victor", "2000-01-01"));
+        updateUser(new User(87, "Andrey", "1989-04-20"));
     }
 
-    private static void requestPosts() {
-        ApiManager.getApiService().getPosts().enqueue(new Callback<List<User>>() {
+    private static void getAllUsers() {
+        ApiManager.getApiService().getAll().enqueue(new Callback<List<User>>() {
             public void onResponse(Call<List<User>> call, Response<List<User>> response) {
                 List<User> users = response.body();
                 if (users != null) {
-                    System.out.println("posts.size: " + users.size());
+                    System.out.println("users: " + users.size());
                     System.out.println(users);
                 }
             }
@@ -34,16 +37,74 @@ public class Main {
         });
     }
 
-    private static void sendPost() {
+    private static void getUser(int id) {
+        ApiManager.getApiService().get(id).enqueue(new Callback<User>() {
+            public void onResponse(Call<User> call, Response<User> response) {
+                User user = response.body();
+                if (user != null) {
+                    System.out.println(user);
+                }
+            }
 
-        //Post post = new Post(1,1, "test title", "test body");
-        User user = new User("Victor", "2000-01-01");
-        System.out.println("User:" + user);
+            public void onFailure(Call<User> call, Throwable throwable) {
+                System.out.println("onFailure: " + throwable);
+            }
+        });
+    }
 
+    private static void getUserByName(String name) {
+        ApiManager.getApiService().getByName(name).enqueue(new Callback<User>() {
+            public void onResponse(Call<User> call, Response<User> response) {
+                User user = response.body();
+                if (user != null) {
+                    System.out.println(user);
+                }
+            }
+
+            public void onFailure(Call<User> call, Throwable throwable) {
+                System.out.println("onFailure: " + throwable);
+            }
+        });
+    }
+
+    private static void getUsersByAge(int fromAge, int toAge) {
+        ApiManager.getApiService().getByAge(fromAge, toAge).enqueue(new Callback<List<User>>() {
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                List<User> users = response.body();
+                if (users != null) {
+                    System.out.println("users: " + users.size());
+                    System.out.println(users);
+                }
+            }
+            public void onFailure(Call<List<User>> call, Throwable throwable) {
+                System.out.println("onFailure: " + throwable);
+            }
+        });
+    }
+
+
+    private static void addUser(User user) {
         try {
-            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            String result = ApiManager.getApiService().sendPost(user).execute().body();
-            System.out.println("result: " + result);
+            ResponseBody responseBody = ApiManager.getApiService().add(user).execute().body();
+            System.out.println(responseBody.string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void updateUser(User user) {
+        try {
+            ResponseBody responseBody = ApiManager.getApiService().update(user).execute().body();
+            System.out.println(responseBody.string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void removeUser(User user) {
+        try {
+            ResponseBody responseBody = ApiManager.getApiService().remove(user).execute().body();
+            System.out.println(responseBody.string());
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/com/company/api/ApiService.java
+++ src/main/java/com/company/api/ApiService.java
@@ -1,21 +1,33 @@
 package com.company.api;
 
-import com.akadatsky.model.Post;
-import com.company.model.SendPostResult;
 import com.company.model.User;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.GET;
-import retrofit2.http.POST;
+import retrofit2.http.*;
 
 import java.util.List;
 
 public interface ApiService {
 
     @GET("get_all")
-    Call<List<User>> getPosts();
+    Call<List<User>> getAll();
+
+    @GET("get/{id}")
+    Call<User> get(@Path("id") int id);
+
+    @GET("get_by_name/{name}")
+    Call<User> getByName(@Path("name") String name);
+
+    @GET("get_by_age/{from_age}/{to_age}")
+    Call<List<User>> getByAge(@Path("from_age") int fromAge, @Path("to_age") int toAge);
 
     @POST("add")
-    Call<String> sendPost(@Body User user);
+    Call<ResponseBody> add(@Body User user);
+
+    @PUT("update")
+    Call<ResponseBody> update(@Body User user);
+
+    @DELETE("remove")
+    Call<ResponseBody> remove(@Body User user);
 
 }
